1c8a0b7f0e85ff87eb9fd984de44d5c8829f1056
Manuel Luna <manuellunada@gmail.com>
2022-06-11 15:20:29 -0500

    enable sessions

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -3,6 +3,7 @@ class RegistrationController < ApplicationController
     user = User.new(user_params)
 
     if user.save
+      session[:user_id] = user.id
       render json: user, status: :created
     else
       render json: user.errors, status: :unprocessable_entity
