fca71375c8ca0d0e2138c580c850867ef46ac7a8
Manuel Luna <manuellunada@gmail.com>
2022-06-11 14:21:58 -0500

    hide digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
 
+  FIELDS_TO_HIDE = [:password_digest].freeze
+
   validates :username,
             presence: true,
             length: { minimum: 3, maximum: 12 },
-            format: { with: /\A[A-Z_0-9.]+\z/i }
+            format: { with: /\A[A-Z_0-9.]+\z/i },
+            uniqueness: true
+
   validates :password_confirmation,
             presence: true
+
+  def as_json(options = {})
+    super(options.merge({ except: FIELDS_TO_HIDE }))
+  end
 end
