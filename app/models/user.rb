b265357a37020a62f15e4d1ccf0b7401e1c2262f
Manuel Luna <manuellunada@gmail.com>
2022-06-11 13:28:37 -0500

    format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :username, presence: true, length: { minimum: 3, maximum: 12 }, format: { with: /\A[A-Z_0-9.]+\z/i }
+  validates :username,
+            presence: true,
+            length: { minimum: 3, maximum: 12 },
+            format: { with: /\A[A-Z_0-9.]+\z/i }
 end
