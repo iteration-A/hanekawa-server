6ad13474a0e83efc4de9a18962720cac73436dad
Manuel Luna <manuellunada@gmail.com>
2022-06-11 17:25:59 -0500

    remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
   post '/sign-up', to: 'registration#signup'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
